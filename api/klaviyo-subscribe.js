b3f5f9e7a0bfed8b7e741abfefd2450b472fcb83
Samuel Fox <samuel.fox2002@outlook.com>
2026-04-28 16:14:16 -0700

    Replace deprecated Klaviyo v2 endpoint with v3 and fix favicon

--- api/klaviyo-subscribe.js
+++ api/klaviyo-subscribe.js
@@ -9,37 +9,55 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: 'Email is required' })
   }
 
-  // eslint-disable-next-line no-undef
-  const listId = process.env.KLAVIYO_LIST_ID
-  // eslint-disable-next-line no-undef
-  const privateKey = process.env.KLAVIYO_PRIVATE_KEY
-
   try {
+    // eslint-disable-next-line no-undef
+    const listId = process.env.KLAVIYO_LIST_ID
+    // eslint-disable-next-line no-undef
+    const privateKey = process.env.KLAVIYO_PRIVATE_KEY
+
     const response = await fetch(
-      `https://a.klaviyo.com/api/v2/list/${listId}/subscribe`,
+      'https://a.klaviyo.com/api/profile-subscription-bulk-create-jobs',
       {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
-          'api-key': privateKey,
+          Authorization: `Klaviyo-API-Key ${privateKey}`,
+          accept: 'application/vnd.api+json',
+          'content-type': 'application/vnd.api+json',
+          revision: '2026-04-15',
         },
         body: JSON.stringify({
-          profiles: [
-            {
-              email: email,
-              tags: ['website-footer-signup'],
+          data: {
+            type: 'profile-subscription-bulk-create-job',
+            attributes: {
+              custom_methods: {
+                email: {
+                  method_type: 'single_opt_in',
+                  tags: ['website-footer-signup'],
+                },
+              },
             },
-          ],
+            relationships: {
+              list: { data: { type: 'list', id: listId } },
+              profiles: {
+                data: [
+                  {
+                    type: 'profile',
+                    attributes: { email },
+                  },
+                ],
+              },
+            },
+          },
         }),
       }
     )
 
-    const data = await response.json()
     if (response.ok) {
       return res.status(200).json({ success: true })
-    } else {
-      return res.status(response.status).json({ error: data })
     }
+
+    const errorBody = await response.text()
+    return res.status(response.status).json({ error: errorBody })
   } catch {
     return res.status(500).json({ error: 'Internal server error' })
   }
