f3858ac7f06b6d5be1c5ce726779838d062b1cf5
Samuel Fox <samuel.fox2002@outlook.com>
2026-04-29 08:40:06 -0700

    Move Klaviyo script to index.html, simplify newsletter component

--- src/components/NewsletterSignup.jsx
+++ src/components/NewsletterSignup.jsx
@@ -1,23 +1,6 @@
-import { useEffect } from 'react'
-
 const NewsletterSignup = () => {
-  useEffect(() => {
-    // Load Klaviyo tracking script once
-    const scriptId = 'klaviyo-script'
-    if (!document.getElementById(scriptId)) {
-      const script = document.createElement('script')
-      script.id = scriptId
-      script.type = 'text/javascript'
-      script.async = true
-      script.src =
-        'https://static.klaviyo.com/onsite/js/TQDY7Q/klaviyo.js?company_id=TQDY7Q'
-      document.head.appendChild(script)
-    }
-  }, [])
-
   return (
     <div className="newsletter-signup">
-      {/* Klaviyo embedded form placeholder */}
       <div className="klaviyo-form-SZeJQr"></div>
     </div>
   )
